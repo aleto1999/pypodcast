f55e7a70b69197e8093d9d9511b7c847bfd39707
aleto1999 <alexleto14@gmail.com>
2026-09-04 11:07:36 -0600

    updated uv command

--- slurm/download.sh
+++ slurm/download.sh
@@ -8,8 +8,4 @@ export HF_HOME="$SLURM_SCRATCH/cache/HF"
 export TORCH_HOME=/scratch/alpine/alle5715/.cache
 export MPLCONFIGDIR=/scratch/alpine/alle5715/.cache
 
-uv sync
-# uv pip install flash-attn --no-build-isolation
-
-
-# uv run python -m podcast_downloader download "The Daily" --max-episodes 10
+uv sync --extra gpu
